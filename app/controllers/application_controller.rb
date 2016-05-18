20c12a3599c93d13f1308f06b2ac913cc00483c9
darktef <darktef@gmail.com>
2016-05-18 17:08:47 -0500

    landing page without sign-in & params usage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
-	before_filter :authenticate_user!
+	before_filter :authenticate_user!, :except => [:welcome]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
