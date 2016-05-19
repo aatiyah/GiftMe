8d73c7bf99c3ab20c8e920f0063c0900df42dbd8
darktef <darktef@gmail.com>
2016-05-19 09:16:21 -0500

    add feeds tracking wishlists actions

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,7 @@
 class Wishlist < ActiveRecord::Base
+	include PublicActivity::Model
+	tracked owner: Proc.new{ |controller, model| controller.current_user }
+
 	validates :title, presence: true
 	belongs_to :user
 	has_many :product
