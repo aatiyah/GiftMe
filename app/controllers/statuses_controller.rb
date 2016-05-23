e274ed3b26fa7ac7ed309e145f43f07c57376b8c
darktef <darktef@gmail.com>
2016-05-22 20:25:03 -0500

    refactor status

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,14 +1,9 @@
 class StatusesController < ApplicationController
-  before_action :set_status, only: [:show, :edit, :update, :destroy]
 
   # GET /statuses/new
   def new
     @user = User.find(params[:id])
     @status = Status.new
-  end
-
-  # GET /statuses/1/edit
-  def edit
   end
 
   # POST /statuses
@@ -19,44 +14,14 @@ class StatusesController < ApplicationController
     respond_to do |format|
       if @status.save
         format.html { redirect_to :back, notice: 'Status was successfully created.' }
-        format.json { render :show, status: :created, location: @status }
       else
         format.html { render :new }
-        format.json { render json: @status.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /statuses/1
-  # PATCH/PUT /statuses/1.json
-  def update
-    respond_to do |format|
-      if @status.update(status_params)
-        format.html { redirect_to @status, notice: 'Status was successfully updated.' }
-        format.json { render :show, status: :ok, location: @status }
-      else
-        format.html { render :edit }
-        format.json { render json: @status.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /statuses/1
-  # DELETE /statuses/1.json
-  def destroy
-    @status.destroy
-    respond_to do |format|
-      format.html { redirect_to statuses_url, notice: 'Status was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_status
-      @status = Status.find(params[:id])
-    end
-
+   
     # Never trust parameters from the scary internet, only allow the white list through.
     def status_params
       params.require(:status).permit(:status)
