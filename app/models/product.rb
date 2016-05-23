b13580654f6ba3b072a54f704ef2db058d286321
darktef <darktef@gmail.com>
2016-05-23 15:37:02 -0500

    serach for diff database

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
 
 	def self.search(search)
 	  if search
-	    where('title LIKE ?', "%#{search}%")
+	    where('title ILIKE ?', "%#{search}%")
 	  else
 	    all
 	  end
