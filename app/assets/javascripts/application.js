81f6aa4c09c473123d51d3fbfdbe74fd6f0cd780
Ahmad Atiyah <medatiyah@gmail.com>
2016-05-23 11:32:45 -0500

    adding modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,9 @@
 // Initialize collapse button
 
 $( document ).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+
 	// Initialize collapse button
   	$(".button-collapse").sideNav();
   	// Initialize collapsible (uncomment the line below if you use the dropdown variation)
@@ -45,6 +48,4 @@ $( document ).ready(function(){
 	    selectYears: 15 // Creates a dropdown of 15 years to control year
 	});
 	$('input#input_text, textarea#textarea1').characterCounter();
-    $('.modal-trigger').leanModal();
-    
 });
