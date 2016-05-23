ede71d3666b156eeed090dbf80e8874212ffd091
Ahmad Atiyah <medatiyah@gmail.com>
2016-05-23 14:21:51 -0500

    Event/annual dsiplay fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,7 +45,7 @@ $( document ).ready(function(){
 	$('.carousel').carousel('prev', [4]); // Move prev n times.
 	$('.datepicker').pickadate({
 	    selectMonths: true, // Creates a dropdown to control month
-	    selectYears: 15 // Creates a dropdown of 15 years to control year
+	    selectYears: 200 // Creates a dropdown of 15 years to control year
 	});
 	$('input#input_text, textarea#textarea1').characterCounter();
 });
