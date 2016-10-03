ad6e2b5540a2df90746af6697a86fabb47b0f5ed
Ahmad Atiyah <medatiyah@gmail.com>
2016-10-02 19:59:19 -0500

    Added .permit to resolve devise issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, 
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, 
     :password_confirmation, :name, :username, :birthday, :avatar_img, :first_name, :last_name) }
 
-   devise_parameter_sanitizer.for(:account_update) { |u| u.permit({ roles: [] }, :email, :password, 
+   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit({ roles: [] }, :email, :password, 
    :password_confirmation, :current_password, :name, :username, :birthday, :avatar_img, :first_name, :last_name) }
   end
 end
